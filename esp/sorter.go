6f22eec6fae4c25718c62f41a5a681b851f85cb5
foliet <foliet@rainspace.cn>
2023-04-16 02:34:09 +0800

    feat: 新增score sorter

--- esp/sorter.go
+++ esp/sorter.go
@@ -17,6 +17,11 @@ type (
 	IdSorter struct {
 		ID string `json:"_id"`
 	}
+
+	ScoreSorter struct {
+		Score_ float64 `json:"_score"`
+		ID     string  `json:"_id"`
+	}
 )
 
 func (s *IdSorter) MakeSortOptions(backward bool) ([]types.SortCombinations, []types.FieldValue, error) {
@@ -45,4 +50,46 @@ func (s *IdSorter) MakeSortOptions(backward bool) ([]types.SortCombinations, []t
 		})
 	}
 	return sort, []types.FieldValue{id}, nil
+}
+
+func (s *ScoreSorter) MakeSortOptions(backward bool) ([]types.SortCombinations, []types.FieldValue, error) {
+	var id string
+	var score float64
+	if s == nil {
+		if backward {
+			id = primitive.NewObjectIDFromTimestamp(time.Unix(0, 0)).Hex()
+			score = 0
+		} else {
+			id = primitive.NewObjectIDFromTimestamp(time.Unix(math.MaxInt64, 0)).Hex()
+			score = math.MaxFloat64
+		}
+	} else {
+		id = s.ID
+		score = s.Score_
+	}
+	var sort []types.SortCombinations
+	if !backward {
+		sort = append(sort, types.SortOptions{
+			SortOptions: map[string]types.FieldSort{
+				"_score": {Order: &sortorder.Desc},
+			},
+		})
+		sort = append(sort, types.SortOptions{
+			SortOptions: map[string]types.FieldSort{
+				"_id": {Order: &sortorder.Desc},
+			},
+		})
+	} else {
+		sort = append(sort, types.SortOptions{
+			SortOptions: map[string]types.FieldSort{
+				"_score": {Order: &sortorder.Asc},
+			},
+		})
+		sort = append(sort, types.SortOptions{
+			SortOptions: map[string]types.FieldSort{
+				"_id": {Order: &sortorder.Asc},
+			},
+		})
+	}
+	return sort, []types.FieldValue{score, id}, nil
 }
